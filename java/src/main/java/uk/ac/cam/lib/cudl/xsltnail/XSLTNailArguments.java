eeab41a2b6dd03b3b0f3e8e2545cbc7c39a7ad72
Hal Blackburn <hwtb2@cam.ac.uk>
2019-09-09 17:04:00 +0100

    Rework cmdline arg parsing
    This implements some simple parser combinators for parsing command line
    args. This should make it easy to add options in the future without
    having to modify ad-hoc parsing code, or verify that a 3rd party CLI
    parsing lib is actually thread safe.

--- java/src/main/java/uk/ac/cam/lib/cudl/xsltnail/XSLTNailArguments.java
+++ java/src/main/java/uk/ac/cam/lib/cudl/xsltnail/XSLTNailArguments.java
@@ -1,32 +1,225 @@
 package uk.ac.cam.lib.cudl.xsltnail;
 
+import io.vavr.Predicates;
+import io.vavr.Tuple;
 import io.vavr.Tuple2;
-import io.vavr.collection.List;
-import io.vavr.collection.Map;
-import io.vavr.collection.Set;
-import io.vavr.collection.Stream;
+import io.vavr.collection.*;
 import io.vavr.control.Either;
+import io.vavr.Predicates;
+import io.vavr.control.Option;
 
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.OptionalInt;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import static io.vavr.API.*;
-import static io.vavr.Predicates.*;
+import static io.vavr.Predicates.isIn;
+import static io.vavr.Predicates.anyOf;
+import static io.vavr.Predicates.exists;
+import static uk.ac.cam.lib.cudl.xsltnail.XSLTNailArguments.Parsers.*;
 
 class XSLTNailArguments {
 
     private static final Set<String> HELP_OPTIONS = List.of("-h", "--help").toSet();
     private static final Set<String> VERSION_OPTIONS = List.of("-v", "--version").toSet();
+    private static final Set<String> VALUE_OPTIONS = List.of("--system-identifier").toSet();
 
     private static final Predicate<String> IS_HELP_OPT = isIn("-h", "--help");
     private static final Predicate<String> IS_VERSION_OPT = isIn("--version");
     private static final Predicate<String> IS_KNOWN_OPT = anyOf(IS_HELP_OPT, IS_VERSION_OPT);
 
+    interface Parser {
+        Either<String, Tuple2<List<Tuple2<String, Object>>, List<String>>>
+        parse(List<Tuple2<String, Object>> values, List<String> args);
+
+        default Either<String, Tuple2<List<Tuple2<String, Object>>, List<String>>> parse(List<String> args) {
+            return parse(List.empty(), args);
+        }
+    }
+
+    static final class Parsers {
+
+        public static Parser constant(String value) {
+            return (List<Tuple2<String, Object>> values, List<String> args) ->
+                args.length() > 0 && value.equals(args.get(0)) ?
+                    success(recordValue(values, value, true), args.drop(1)) : failure(value);
+        }
+
+        public static Parser option(String name) { return option(name, name); }
+        public static Parser option(String name, String key) {
+            return (List<Tuple2<String, Object>> values, List<String> args) ->
+                constant(name).parse(List.empty(), args)
+                    .map(result -> result.update1(recordValue(values, key, true)));
+        }
+
+        public static Parser optionWithValue(String name) { return optionWithValue(name, name); }
+        public static Parser optionWithValue(String name, String key) {
+            String nameWithEquals = name + "=";
+            return (List<Tuple2<String, Object>> values, List<String> args) -> {
+                if(args.length() > 0 && args.get(0).startsWith(nameWithEquals)) {
+                    String value = args.get(0).substring(nameWithEquals.length());
+                    return success(recordValue(values, key, value), args.drop(1));
+                }
+                else if(args.length() > 1 && args.get(0).equals(name)) {
+                    return success(recordValue(values, key, args.get(1)), args.drop(2));
+                }
+                return failure(nameWithEquals);
+            };
+        }
+
+        public static Parser value(String key) {
+            return (List<Tuple2<String, Object>> values, List<String> args) ->
+                args.length() > 0 ? success(recordValue(values, key, args.get(0)), args.drop(1)) : failure(key);
+        }
+
+        public static Parser regex(String pattern, String key) {
+            Pattern p = Pattern.compile(pattern);
+            return (List<Tuple2<String, Object>> values, List<String> args) ->
+                !args.isEmpty() && p.matcher(args.get(0)).matches() ?
+                    success(recordValue(values, key, args.get(0)), args.drop(1)) : failure(key);
+        }
+
+        public static Parser any() {
+            return (List<Tuple2<String, Object>> values, List<String> args) ->
+                value("*").parse(values, args).flatMap(result -> success(values, result._2));
+        }
+
+        public static Parser allOf(Parser... parsers) {
+            return allOf(List.of(parsers));
+        }
+
+        public static Parser allOf(List<Parser> parsers) {
+            return (List<Tuple2<String, Object>> values, List<String> args) ->
+                parsers.foldLeft(success(values, args), (prev, parser) ->
+                    prev.flatMap(_prev -> parser.parse(_prev._1, _prev._2)));
+        }
+
+        public static Parser firstOf(Parser... parsers) {
+            return firstOf(List.of(parsers));
+        }
+
+        public static Parser firstOf(List<Parser> parsers) {
+            return (List<Tuple2<String, Object>> values, List<String> args) ->
+                parsers.isEmpty() ? failure("firstOf([])") :
+                parsers.toStream().map(p -> p.parse(values, args)).zipWithIndex()
+                    .filter(entry -> entry._1.isRight() || entry._2 == parsers.length() - 1)
+                    .map(entry -> entry._1)
+                    .get();
+        }
+
+        public static Parser times(Parser parser, int min) { return times(parser, min, OptionalInt.empty()); }
+        public static Parser times(Parser parser, int min, int max) { return times(parser, min, OptionalInt.of(max)); }
+        public static Parser times(Parser parser, int min, OptionalInt max) {
+            Objects.requireNonNull(max, "max cannot be null");
+            if(min < 0)
+                throw new IllegalArgumentException(String.format("min must be >= 0; min=%d", min));
+            if(max.isPresent() && max.getAsInt() < min)
+                throw new IllegalArgumentException(String.format("max must be >= min; min=%d, max=%d", min, max.getAsInt()));
+
+            return (List<Tuple2<String, Object>> values, List<String> args) -> {
+                Either<String, Tuple2<List<Tuple2<String, Object>>, List<String>>> result = failure("times(*, 0, 0)");
+                for(int i = 0; !max.isPresent() || i < max.getAsInt(); ++i) {
+                    result = parser.parse(values, args);
+                    if(result.isLeft())
+                        return i >= min ? success(values, args) : result;
+                    values = result.get()._1;
+                    args = result.get()._2;
+                }
+                return result;
+            };
+        }
+
+        public static Parser opt(Parser parser) {
+            return times(parser, 0, 1);
+        }
+
+        public static Parser not(Parser parser, String desc) {
+            return (List<Tuple2<String, Object>> values, List<String> args) -> {
+                Either<String, Tuple2<List<Tuple2<String, Object>>, List<String>>> result = parser.parse(values, args);
+                if(result.isRight())
+                    return failure(desc);
+                // Don't consume any input
+                return success(values, args);
+            };
+        }
+
+        private static List<Tuple2<String, Object>> recordValue(List<Tuple2<String, Object>> values, String key, Object value) {
+            return values.prepend(Tuple.of(key, value));
+        }
+
+        private static Either<String, Tuple2<List<Tuple2<String, Object>>, List<String>>>
+            success(List<Tuple2<String, Object>> context, List<String> remaining) {
+            return Either.right(Tuple.of(context, remaining));
+        }
+
+        private static Either<String, Tuple2<List<Tuple2<String, Object>>, List<String>>>
+        failure(String failure) {
+            return Either.left(failure);
+        }
+    }
+
     private static final Map<String, Object> DEFAULT_RESULT = Map(
+        "--", false,
         "--help", false,
         "--version", false,
+        "--system-identifier", null,
         "transform", false,
         "<xslt-file>", null,
-        "<xml-base-uri>", null);
+        "<xml-file>", null);
+
+    private static class XSLTNailArgumentsParser {
+        private XSLTNailArgumentsParser() {}
+
+        private static final Parser unambiguousValue(String key) { return regex("^(?:|(?!--).+)$", key); }
+
+        private static final Parser HELP_OPTION = firstOf(option("--help"), option("-h", "--help"));
+        private static final Parser VERSION_OPTION = option("--version");
+
+
+        private static final Parser SUBCOMMAND_TRANSFORM = constant("transform");
+        private static final Parser ARG_SEPARATOR = constant("--");
+        private static final Parser SYSTEM_ID_OPTION = optionWithValue("--system-identifier");
+        private static final Parser OPTIONS = opt(firstOf(SYSTEM_ID_OPTION));
+        private static final Parser XSLT_FILE = value("<xslt-file>");
+        private static final Parser XML_FILE = value("<xml-file>");
+        private static final Parser NOT_OPTLIKE_XSLT_FILE = unambiguousValue("<xslt-file>");
+        private static final Parser NOT_OPTLIKE_XML_FILE = unambiguousValue("<xml-file>");
+
+        private static final Parser TRANSFORM = firstOf(
+            allOf(SUBCOMMAND_TRANSFORM, OPTIONS, NOT_OPTLIKE_XSLT_FILE, OPTIONS, opt(allOf(NOT_OPTLIKE_XML_FILE, OPTIONS))),
+            // Don't allow options after the -- argument separator
+            allOf(SUBCOMMAND_TRANSFORM, OPTIONS, ARG_SEPARATOR, XSLT_FILE, opt(XML_FILE)));
+
+        private static final Parser HELP = allOf(
+            times(allOf(not(HELP_OPTION, "not --help"), any()), 0),
+            HELP_OPTION, times(any(), 0));
+
+        private static final Parser VERSION = allOf(
+            times(allOf(not(VERSION_OPTION, "not --version"), any()), 0),
+            VERSION_OPTION, times(any(), 0));
+
+        private static final Parser ROOT = firstOf(TRANSFORM, HELP, VERSION);
+    }
+
+    public static Either<Option<String>, Map<String, Object>> _parse(String... args) { return _parse(List.of(args)); }
+    public static Either<Option<String>, Map<String, Object>> _parse(List<String> args) {
+        return XSLTNailArgumentsParser.ROOT.parse(args)
+            .fold(str -> Either.left(Option.none()),
+                  result -> result._2.isEmpty() ?
+                      mergeValues(result._1).mapLeft(Option::some) : Either.left(Option.none()));
+    }
+
+    private static Either<String, Map<String, Object>> mergeValues(List<Tuple2<String, Object>> values) {
+        Map<String, Object> result = Map();
+        for(Tuple2<String, Object> value : values) {
+            if(result.containsKey(value._1))
+                return Either.left(String.format("duplicate argument: %s", value._1));
+            result = result.put(value);
+        }
+        return Either.right(result.merge(DEFAULT_RESULT));
+    }
 
     /**
      * Parse a command line argument list conforming to {@link Constants#USAGE_TRANSFORM_FULL}.
@@ -45,7 +238,7 @@ class XSLTNailArguments {
             Case($(exists(IS_VERSION_OPT)),
                 Either.right(withDefaults(Map("--version", true)))),
             // We don't have any
-            Case($(exists(allOf(XSLTNailArguments::isOption, not(IS_KNOWN_OPT)))),
+            Case($(exists(Predicates.allOf(XSLTNailArguments::isOption, Predicates.not(IS_KNOWN_OPT)))),
                 Either.left(Constants.USAGE_TRANSFORM)),
             Case($(), () -> extractValues(args))
         );
@@ -56,6 +249,7 @@ class XSLTNailArguments {
     }
 
     private static List<String> getOptionCandidates(List<String> args) {
+//        Tuple2<List<String>, List<String>> split = args.splitAt(3);
         Tuple2<List<String>, List<String>> split = args.splitAt(isIn("--"));
         // The first half contains all the elements if "--" does not occur
         return split._2().isEmpty() ? split._1(): List.empty();
@@ -63,7 +257,7 @@ class XSLTNailArguments {
 
     private static List<String> getPositionals(List<String> args) {
         return args.splitAt(isIn("--")).apply((a, b) -> {
-            a = a.filter(not(XSLTNailArguments::isOption));
+            a = a.filter(Predicates.not(XSLTNailArguments::isOption));
             return b.isEmpty() ? a : Stream.concat(a, b.slice(1, b.length())).toList();
         });
     }
--- java/src/test/groovy/uk/ac/cam/lib/cudl/xsltnail/XSLTNailArgumentsSpec.groovy
+++ java/src/test/groovy/uk/ac/cam/lib/cudl/xsltnail/XSLTNailArgumentsSpec.groovy
@@ -1,8 +1,15 @@
-package uk.ac.cam.lib.cudl.xsltnail;
+package uk.ac.cam.lib.cudl.xsltnail
 
-import org.docopt.DocoptExitException;
+import io.vavr.Tuple
+import io.vavr.collection.HashMap
+import io.vavr.collection.List
+import io.vavr.collection.Map
+import io.vavr.control.Either
+import io.vavr.control.Option
 import spock.lang.Specification;
-import spock.lang.Unroll;
+import spock.lang.Unroll
+
+import static uk.ac.cam.lib.cudl.xsltnail.XSLTNailArguments.Parsers.*
 
 class XSLTNailArgumentsSpec extends Specification {
     @Unroll
@@ -53,5 +60,281 @@ class XSLTNailArgumentsSpec extends Specification {
             "transform /foo",
             "transform /foo /bar /baz",
         ].collect { it.split() }
+    }
+
+    @Unroll
+    def "constant() matches constant value"(context, args, expected) {
+        given:
+        def parser = constant("foo")
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["foo"], [[["foo", true]], []]],
+            [[["abc", true]], ["foo", "bar"], [[["foo", true], ["abc", true]], ["bar"]]],
+            [[], ["bar"], "foo"],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "value() matches any value"(context, args, expected) {
+        given:
+        def parser = value("<foo>")
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["foo"], [[["<foo>", "foo"]], []]],
+            [[["abc", true]], ["foo", "bar"], [[["<foo>", "foo"], ["abc", true]], ["bar"]]],
+            [[], [], "<foo>"],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "option() matches long option"(context, args, expected) {
+        given:
+        def parser = option("--foo", "--foo")
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["--foo"], [[["--foo", true]], []]],
+            [[["abc", true]], ["--foo", "bar"], [[["--foo", true], ["abc", true]], ["bar"]]],
+            [[], ["bar"], "--foo"],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "option() matches short option"(context, args, expected) {
+        given:
+        def parser = option("-f", "--foo")
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["-f"], [[["--foo", true]], []]],
+            [[["abc", true]], ["-f", "bar"], [[["--foo", true], ["abc", true]], ["bar"]]],
+            [[], ["bar"], "-f"],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "optionWithValue() matches long option with value"(context, args, expected) {
+        given:
+        def parser = optionWithValue("--foo", "--foo")
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["--foo", "123"], [[["--foo", "123"]], []]],
+            [[], ["--foo=123"], [[["--foo", "123"]], []]],
+            [[["abc", true]], ["--foo", "123", "bar"], [[["--foo", "123"], ["abc", true]], ["bar"]]],
+            [[["abc", true]], ["--foo=123", "bar"], [[["--foo", "123"], ["abc", true]], ["bar"]]],
+            [[], ["bar"], "--foo="],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "allOf() applies a sequence of parsers"(context, args, expected) {
+        given:
+        def parser = allOf(value("<a>"), constant("foo"), value("<b>"))
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["a", "foo", "b"], [[["<b>", "b"], ["foo", true], ["<a>", "a"]], []]],
+            [[], ["a", "bar", "b"], "foo"],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "allOf([]) is successful"(context, args, expected) {
+        given:
+        def parser = allOf()
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], [], [[], []]],
+            [[["foo", true]], ["abc"], [[["foo", true]], ["abc"]]]
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "firstOf([]) is unsuccessful"(context, args, expected) {
+        given:
+        def parser = firstOf()
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], [], "firstOf([])"],
+            [[["foo", true]], ["abc"], "firstOf([])"]
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "firstOf() uses the first matching parser result"(context, args, expected) {
+        given:
+        def parser = firstOf(constant("foo"), constant("bar"))
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["foo"], [[["foo", true]], []]],
+            [[], ["bar"], [[["bar", true]], []]],
+            [[], ["bar", "bar"], [[["bar", true]], ["bar"]]],
+            [[], ["xyz"], "bar"],
+            [[], [], "bar"],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "times(*, 0, 0) never matches"(context, args, expected) {
+        given:
+        def parser = times(value("<foo>"), 0, 0)
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["foo"], "times(*, 0, 0)"],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "times(*, 0) matches parser 0 or more times"(context, args, expected) {
+        given:
+        def parser = times(constant("x"), 0)
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], [], [[], []]],
+            [[], ["y"], [[], ["y"]]],
+            [[], ["x"], [[["x", true]], []]],
+            [[], ["x", "x"], [[["x", true], ["x", true]], []]],
+            [[], ["x", "x", "y"], [[["x", true], ["x", true]], ["y"]]],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "times(*, n) matches parser n or more times"(context, args, expected) {
+        given:
+        def parser = times(constant("x"), 2)
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["x", "y"], "x"],
+            [[], ["x", "x", "y"], [[["x", true], ["x", true]], ["y"]]],
+            [[], ["x", "x", "x", "y"], [[["x", true], ["x", true], ["x", true]], ["y"]]],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    @Unroll
+    def "times(*, n, o) matches parser n - o times"(context, args, expected) {
+        given:
+        def parser = times(constant("x"), 2, 3)
+
+        when:
+        def result = parser.parse(context, args)
+        then:
+        result == expected
+
+        where:
+        [context, args, expected] << [
+            [[], ["x", "y"], "x"],
+            [[], ["x", "x", "y"], [[["x", true], ["x", true]], ["y"]]],
+            [[], ["x", "x", "x", "y"], [[["x", true], ["x", true], ["x", true]], ["y"]]],
+            [[], ["x", "x", "x", "x", "y"], [[["x", true], ["x", true], ["x", true]], ["x", "y"]]],
+        ].collect(this.&parserParamsToVavr)
+    }
+
+    private static java.util.List<?> parserParamsToVavr(java.util.List<?> params) {
+        return [
+            List.ofAll(params[0].collect { Tuple.of(it[0], it[1]) }),
+            List.ofAll(params[1]),
+            params[2] instanceof String ? Either.left(params[2]) :
+                Either.right(Tuple.of(
+                    List.ofAll(params[2][0].collect { Tuple.of(it[0], it[1]) }),
+                    List.ofAll(params[2][1])))
+        ]
+    }
+
+    @Unroll
+    def "parse()"(args, expected) {
+        when:
+        def result = XSLTNailArguments._parse(args)
+
+        then:
+        result == expected
+
+        where:
+        [args, expected] << [
+            // FIXME: Need to ensure --system-identifier or <xml-file> are provided
+            [["transform", "foo", "bar"],
+             ["--": false, "--help": false, "--version": false, "transform": true,
+              "--system-identifier": null, "<xslt-file>": "foo", "<xml-file>": "bar"]],
+            [["transform", "--system-identifier", "abc", "foo", "bar"],
+             ["--": false, "--help": false, "--version": false, "transform": true,
+              "--system-identifier": "abc", "<xslt-file>": "foo", "<xml-file>": "bar"]],
+            [["transform", "--system-identifier=abc", "foo", "bar"],
+             ["--": false, "--help": false, "--version": false, "transform": true,
+              "--system-identifier": "abc", "<xslt-file>": "foo", "<xml-file>": "bar"]],
+            [["transform", "--system-identifier=abc", "--", "--foo", "--bar"],
+             ["--": true, "--help": false, "--version": false, "transform": true,
+              "--system-identifier": "abc", "<xslt-file>": "--foo", "<xml-file>": "--bar"]],
+        ].collect { [List.ofAll(it[0]),
+                     it[1] instanceof java.util.Map ?
+                         Either.right(HashMap.ofAll(it[1])) :
+                         Either.left(it[1] == null ? Option.none() : Option.some(it[1]))] }
     }
 }
