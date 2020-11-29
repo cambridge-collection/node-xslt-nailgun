373d322ab68dd6aaa18526bc5292f54542f3027f
Hal Blackburn <hwtb2@cam.ac.uk>
2020-11-29 18:08:47 +0000

    fix: Correct type error in _internals.timeout()

--- test/_internals.test.ts
+++ test/_internals.test.ts
@@ -1,5 +1,10 @@
 import path from 'path';
-import {AddressType, getClasspath, parseClarkName} from '../src/_internals';
+import {
+  AddressType,
+  getClasspath,
+  parseClarkName,
+  timeout,
+} from '../src/_internals';
 
 test('AddressType enum has string value', () => {
   expect(AddressType.local).toBe('local');
@@ -46,4 +51,22 @@ test.each([
   ],
 ])('parseClarkName() rejects invalid Clark qname %s', (value, msg) => {
   expect(() => parseClarkName(value)).toThrow(msg);
+});
+
+describe('timeout()', () => {
+  test('value is optional', async () => {
+    let value: string | undefined = undefined;
+    await expect(timeout(10)).resolves.toBeUndefined();
+    await expect(timeout(10, value)).resolves.toBeUndefined();
+    value = 'abc';
+    await expect(timeout(10, value)).resolves.toBe('abc');
+  });
+
+  test('close() stops the timeout immediately', async () => {
+    jest.useFakeTimers();
+    const t = timeout(1000 * 60, 42);
+    setTimeout(t.close, 1000);
+    jest.advanceTimersByTime(2000);
+    await expect(t).resolves.toBe(42);
+  }, 2000);
 });
